644c58a6b5f6cfc58351711eb8df16bbf891e826
tandrima3101 <goswami3101@gmail.com>
2022-09-01 11:57:48 +0530

    transfer repo

--- src/components/Video.js
+++ src/components/Video.js
@@ -14,13 +14,13 @@ function Video(props) {
         {
           props.videoDetails.map((x,index) => {
             return (
-              <section className="intro-video" style={{ position: "relative" }}>
+              <section className="intro-video" style={{position:'relative'}}>
                 <div className="col-lg-12">
                   <iframe
                     width="560"
                     height="315"
                     controls='false'
-                    src={x.link}
+                    src="https://www.youtube.com/embed/JHlY8w69wSE"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     style={{ height: "80vh", width: "100%" }}
@@ -40,9 +40,11 @@ function Video(props) {
                   className="col-lg-7"
                   style={{
                     position: "absolute",
-                    top: "15%",
-                    right: "10%",
+                    top: "0%",
+                    right: "0%",
                     width: "43%",
+                    zIndex:'9999',
+                    height:'100%'
                   }}
                 >
                   <div key={`ind-${index}`} className="intro-content-box intro-content-box-one">
